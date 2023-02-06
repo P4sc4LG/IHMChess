3c62775b26c13d0b92810dd31bf0ecceb648c019
pascal <dupascal00@gmail.com>
2023-02-06 01:50:35 +0100

    Add background & style

--- src/App.java
+++ src/App.java
@@ -11,12 +11,20 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import static javafx.scene.layout.BackgroundPosition.CENTER;
+import static javafx.scene.layout.BackgroundRepeat.*;
+import static javafx.scene.layout.BackgroundSize.*;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -26,7 +34,9 @@ public class App extends Application{
     @Override
     public void start(Stage stage){
         Button startButton = new Button("Start");
+        startButton.setId("button-style");
         Button leaveButton = new Button("Quit");
+        leaveButton.setId("button-style");
         /*btn.setOnAction(e -> {
             StackPane root2 = new StackPane();
             Label label = new Label("Second form");
@@ -42,6 +52,7 @@ public class App extends Application{
 
             HBox joueurWhite = new HBox(); 
             Label labelWhite = new Label();
+            labelWhite.setId("text-style-joueur");
             labelWhite.setText("Nom du joueur Blanc : ");
             TextField textFieldWhite = new TextField();
             joueurWhite.getChildren().addAll(labelWhite,textFieldWhite);
@@ -49,11 +60,14 @@ public class App extends Application{
 
             HBox joueurBlack = new HBox(); 
             Label labelBlack = new Label();
+            labelBlack.setId("text-style-joueur");
             labelBlack.setText("Nom du joueur Noir : ");
             TextField textFieldBlack = new TextField();
             joueurBlack.getChildren().addAll(labelBlack,textFieldBlack);
             
+
             Button submitButton = new Button("Valider");
+            submitButton.setId("button-style-submit");
             submitButton.setOnAction(e -> {
                 if(textFieldBlack.getText().trim().equals("") 
                 || textFieldWhite.getText().trim().equals("")){
@@ -72,7 +86,7 @@ public class App extends Application{
                     Label blackLabel = new Label("Joueur Noir : "+jBlack);
                     Label whiteLabel = new Label("Joueur Blanc : "+jWhite);
                     
-                    VBox vb = new VBox(blackLabel,whiteLabel);
+                    VBox vb = new VBox(blackLabel,whiteLabel,submitButton);
                     vb.setAlignment(Pos.CENTER);
                     vb.setPadding(new Insets(15,15,15,150));
                     Scene sc = new Scene(vb,600,600);
@@ -81,10 +95,14 @@ public class App extends Application{
                     stage.show();
                 }
             });
-            VBox vb = new VBox(joueurWhite,joueurBlack,submitButton);
-            vb.setAlignment(Pos.CENTER);
-            vb.setPadding(new Insets(15,15,15,150));
-            Scene sc = new Scene(vb,600,600);
+            VBox vChoix = new VBox(joueurWhite,joueurBlack,submitButton);
+            vChoix.setPadding(new Insets(15,15,15,150));
+            vChoix.setBackground(new Background(new BackgroundImage(new Image("background/backgroundJoueur.jpg")
+                                , NO_REPEAT, NO_REPEAT, CENTER, new BackgroundSize(1.5, 1.5, true, true, false, false))));
+            vChoix.setAlignment(Pos.CENTER);
+            vChoix.setSpacing(10);
+            Scene sc = new Scene(vChoix,600,600);
+            sc.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
             stage.setTitle("Création des joueurs");
             stage.setScene(sc);
             stage.show();
@@ -92,13 +110,19 @@ public class App extends Application{
         leaveButton.setOnAction(e -> {
             stage.close();
         });
+        Label titre = new Label();
+        titre.setText("CHESS GAME");
+        titre.setPadding(new Insets(0,0,380,0));
+        titre.setStyle("-fx-text-fill: White;-fx-font-weight:bold");
+        titre.setFont(Font.font("Arial",50));
         VBox vMenu = new VBox();
         
-        vMenu.getChildren().addAll(startButton,leaveButton);
-        vMenu.setSpacing(30);
+        vMenu.getChildren().addAll(titre,startButton,leaveButton);
+        vMenu.setSpacing(10);
         vMenu.setAlignment(Pos.CENTER);
-    
+        vMenu.setId("form-menu");
         Scene sc = new Scene(vMenu,600,600);
+        sc.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
         stage.setTitle("Menu");
         stage.setScene(sc);
         stage.show();  
